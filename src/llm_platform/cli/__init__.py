4457e11d3fa07ec72240b42bf14a235de4727141
jverre <jverre@gmail.com>
2023-09-03 23:15:42 +0100

    First draft

---
+++ src/llm_platform/cli/__init__.py
@@ -0,0 +1,45 @@
+import click
+import sqlite3
+
+@click.group()
+@click.option(
+    "-v",
+    "--verbose",
+    count=True,
+    default=0,
+    help="-v for DEBUG",
+)
+def cli(verbose):
+    print(verbose)
+
+@cli.command("start")
+@click.option(
+    "--option1"
+)
+def start(option1):
+    # Configure sqlite3 platform
+    conn = sqlite3.connect(".llm-platform/")
+    c = conn.cursor()
+
+    # Create empty tables
+    c.execute('''
+    CREATE TABLE IF NOT EXISTS models
+    ([model_id] INTEGER PRIMARY KEY, [model_name] TEXT, [model_type] TEXT)
+    ''')
+
+    # c.execute('''
+    # INSERT INTO models(model_id, model_name, model_type) 
+    # SELECT 'openai_chat_completions', 'openai', 'openai')   
+    # ''')
+    
+    conn.commit()
+
+    # Start server
+    import llm_platform.server as server
+    import uvicorn
+    uvicorn.run(server.app)
+
+    print('start server')
+
+if __name__ == '__main__':
+   cli()
